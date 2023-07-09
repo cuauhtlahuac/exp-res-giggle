69f0df218a41b4316729f19af7103577b9e9f83b
Rodrigo Aristeo <cuitlagua@gmail.com>
2023-07-09 16:53:14 -0600

    adding vite

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// Add Express
-const express = require("express");
-// Initialize Express
-const app = express();
-// Create GET request
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-});
-// Initialize server
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
+/**
+ * This server was made to show how you can send a html file generated
+ * in the famous framework Vite
+ */
+
+// I'm using pnpm as package manager
+const express = require('express')
+const app = express()
+const path = require('path');
+// express automatically search inside the view folder to use ejs
+
+const htmlPath = path.join(__dirname, 'vite-react-giggle', 'dist', 'index.html');
+const assetsPath = path.join(__dirname, 'vite-react-giggle', 'dist');
+const port = 3000
+app.use(express.static(assetsPath));
+app.get('/', (req, res) => {
+    console.log(req);
+    res.sendFile(htmlPath);
+    res.end();
 });
 
-// Export the Express API
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
