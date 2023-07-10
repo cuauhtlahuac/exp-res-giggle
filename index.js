12d6fa7572203f5e36a7be7c0f454f553708c7ad
Rodrigo Aristeo <cuitlagua@gmail.com>
2023-07-10 10:23:31 -0600

    update

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ const path = require('path');
 const htmlPath = path.join(__dirname, 'vite-react-giggle', 'dist', 'index.html');
 const assetsPath = path.join(__dirname, 'vite-react-giggle', 'dist');
 const port = 3000
-app.use(express.static(assetsPath));
-console.log("HTML PATH: ", htmlPath);
-console.log("ASSETS PATH: ", assetsPath);
+// app.use(express.static(assetsPath));
+console.log("HTML - PATH: ", htmlPath);
+console.log("ASSETS - PATH: ", assetsPath);
+
 app.get('/', (req, res) => {
+    res.json({ hola: 'Hola esto es unaprueba', htmlPath, assetsPath })
+    res.end()
+})
+app.use(express.static(assetsPath));
+app.get('/app', (req, res) => {
     res.sendFile(htmlPath);
     res.end();
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
